e82a3970ff21007089cc84e7e41d7e76c2f323c4
OmarAhmed4 <31747623+OmarAhmed4@users.noreply.github.com>
2018-11-14 23:56:52 +0200

    PIechart with linechart
    connect Piechart to line chart

--- MSECG/MSECG/app/src/main/java/com/example/omarahmed/msecg/Piechart_Test.java
+++ MSECG/MSECG/app/src/main/java/com/example/omarahmed/msecg/Piechart_Test.java
@@ -1,33 +1,45 @@
 package com.example.omarahmed.msecg;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.animation.Animation;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class Piechart_Test extends AppCompatActivity implements OnChartValueSelectedListener,OnChartGestureListener,
-        OnChartValueSelectedListener {
+public class Piechart_Test extends AppCompatActivity implements OnChartValueSelectedListener,OnChartGestureListener
+         {
 
     // TODO: Connect The Piechart to Line Chart to detuce the selected lead
     // TODO: COnnect the State OF Each Cycle
+    // TODO: THe PieChart
+
 
     private String[] leads_name= new String[]{"Lead I","Lead II","Lead III","AVR","AVF","AVL","V1","V2","V3","V4","V5","V6"};
     private LineChart mChart;
+
 
 
     @Override
@@ -69,28 +81,45 @@ public class Piechart_Test extends AppCompatActivity implements OnChartValueSele
         pieChart.setData(data);
         dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
         pieChart.setDescription("Leads Selection");
-        pieChart.setDrawHoleEnabled(true);
+        pieChart.setDrawHoleEnabled(true);                  // TODO : Turning on hole of draw
         pieChart.setTransparentCircleRadius(30f);
         pieChart.setHoleRadius(30f);
         pieChart.setOnChartValueSelectedListener(this);
 
         // TODO :   Line Chart
 
-        mChart = (LineChart) findViewById(R.id.line_chart);
+        mChart = (LineChart) findViewById(R.id.lead_line_chart);
         mChart.setOnChartGestureListener(this);
-        mChart.setOnChartValueSelectedListener(this);
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+                Log.i("Entry selected", e.toString());
+                Log.i("LOWHIGH", "low: " + mChart.getLowestVisibleXIndex()
+                        + ", high: " + mChart.getHighestVisibleXIndex());
+
+                Log.i("MIN MAX", "xmin: " + mChart.getXChartMin()
+                        + ", xmax: " + mChart.getXChartMax()
+                        + ", ymin: " + mChart.getYChartMin()
+                        + ", ymax: " + mChart.getYChartMax());
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
         mChart.setDrawGridBackground(false);
 
-        setData();
+       // setData();
         Legend l = mChart.getLegend();
 
         // modify the legend ...
         // l.setPosition(LegendPosition.LEFT_OF_CHART);
-        l.setForm(Legend.LegendForm.LINE);
+        l.setForm(Legend.LegendForm.SQUARE);
 
         // no description text
         mChart.setDescription("Lead Chart");
-        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+        mChart.setNoDataTextDescription("select lead  from PieChart on Top");
 
         // enable touch gestures
         mChart.setTouchEnabled(true);
@@ -101,7 +130,7 @@ public class Piechart_Test extends AppCompatActivity implements OnChartValueSele
         // mChart.setScaleXEnabled(true);
         // mChart.setScaleYEnabled(true);
 
-        LimitLine upper_limit = new LimitLine(130f, "Upper Limit");
+       /* LimitLine upper_limit = new LimitLine(130f, "Upper Limit");
         upper_limit.setLineWidth(4f);
         upper_limit.enableDashedLine(10f, 10f, 0f);
         upper_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
@@ -111,21 +140,20 @@ public class Piechart_Test extends AppCompatActivity implements OnChartValueSele
         lower_limit.setLineWidth(4f);
         lower_limit.enableDashedLine(10f, 10f, 0f);
         lower_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-        lower_limit.setTextSize(10f);
+        lower_limit.setTextSize(10f);*/
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
-        leftAxis.addLimitLine(upper_limit);
-        leftAxis.addLimitLine(lower_limit);
-        leftAxis.setAxisMaxValue(220f);
+        leftAxis.setAxisMaxValue(50f);
         leftAxis.setAxisMinValue(-50f);
         //leftAxis.setYOffset(20f);
-        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.enableGridDashedLine(0f, 0f, 0f);
 
         // limit lines are drawn behind data (and not on top)
-        leftAxis.setDrawLimitLinesBehindData(true);
+        //leftAxis.setDrawLimitLinesBehindData(true);
 
         mChart.getAxisRight().setEnabled(false);
+        mChart.getXAxis().setDrawLabels(false);          //Hide x values
 
         //mChart.getViewPortHandler().setMaximumScaleY(2f);
         //mChart.getViewPortHandler().setMaximumScaleX(2f);
@@ -159,18 +187,147 @@ public class Piechart_Test extends AppCompatActivity implements OnChartValueSele
 
     }
 
+    private ArrayList<String> setXAxisValues(){
+        ArrayList<String> xVals = new ArrayList<String>();
+        for(int i =0;i<100;i++)
+        {
+            xVals.add(String.valueOf(i));
+        }
+
+
+        return xVals;
+    }
+
+    private ArrayList<Entry> setYAxisValues(){
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        Random r=new Random();
+        for(int i =0;i<100;i++)
+        {
+            yVals.add(new Entry(r.nextInt(100)-50,i));
+        }
+       /* yVals.add(new Entry(50, 0));
+        yVals.add(new Entry(20, 1));
+        yVals.add(new Entry(10.5f, 2));
+        yVals.add(new Entry(-5, 3));
+        yVals.add(new Entry(-50.9f, 4));*/
+
+
+        return yVals;
+    }
+
+    private void setData() {
+        ArrayList<String> xVals = setXAxisValues();
+
+        ArrayList<Entry> yVals = setYAxisValues();
+
+        LineDataSet set1;
+
+        // create a dataset and give it a type
+        set1 = new LineDataSet(yVals, "Leads Gragh");
+        set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        //   set1.enableDashedLine(10f, 5f, 0f);
+        // set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setDrawCircles(false);
+        set1.setLineWidth(1f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setDrawFilled(true);                //TODO :  Fillied chart
+
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        mChart.setData(data);
+        mChart.animateX(5000, Easing.EasingOption.EaseInOutQuart);
+        mChart.invalidate();     // to refresh the chart   to update the chart
+
+    }
+
+
+    @Override
+    public void onChartGestureStart(MotionEvent me,
+                                    ChartTouchListener.ChartGesture
+                                            lastPerformedGesture) {
+
+        Log.i("Gesture", "START, x: " + me.getX() + ", y: " + me.getY());
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me,
+                                  ChartTouchListener.ChartGesture
+                                          lastPerformedGesture) {
+
+        Log.i("Gesture", "END, lastGesture: " + lastPerformedGesture);
+
+        // un-highlight values after the gesture is finished and no single-tap
+        if(lastPerformedGesture != ChartTouchListener.ChartGesture.SINGLE_TAP)
+            // or highlightTouch(null) for callback to onNothingSelected(...)
+            mChart.highlightValues(null);
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+        Log.i("LongPress", "Chart longpressed.");
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+        Log.i("DoubleTap", "Chart double-tapped.");
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+        Log.i("SingleTap", "Chart single-tapped.");
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2,
+                             float velocityX, float velocityY) {
+        Log.i("Fling", "Chart flinged. VeloX: "
+                + velocityX + ", VeloY: " + velocityY);
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+        Log.i("Scale / Zoom", "ScaleX: " + scaleX + ", ScaleY: " + scaleY);
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+        Log.i("Translate / Move", "dX: " + dX + ", dY: " + dY);
+    }
+
+
+
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
+        mChart.clear();
+    }
+
+
 
     @Override
     public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
 
-        Toast t=Toast.makeText(getApplicationContext(),"Selected Lead is :"+leads_name[dataSetIndex] ,Toast.LENGTH_SHORT);
+        Toast t=Toast.makeText(getApplicationContext(),"Selected Lead is : "+ leads_name[e.getXIndex()] ,Toast.LENGTH_SHORT);
         t.show();
+        mChart.clear();
+        setData();
 
 
-    }
-
-    @Override
-    public void onNothingSelected() {
 
     }
+
+
 }
