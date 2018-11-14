b91222d6800e6b43d33e78366e6ee1ae7bec1340
OmarAhmed4 <31747623+OmarAhmed4@users.noreply.github.com>
2018-11-14 21:38:25 +0200

    Adding PIeChart
    after adding the piechart to select the lead that will be showen to the user

--- MSECG/MSECG/app/src/main/java/com/example/omarahmed/msecg/Piechart_Test.java
+++ MSECG/MSECG/app/src/main/java/com/example/omarahmed/msecg/Piechart_Test.java
@@ -2,25 +2,33 @@ package com.example.omarahmed.msecg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class Piechart_Test extends AppCompatActivity implements OnChartValueSelectedListener {
+public class Piechart_Test extends AppCompatActivity implements OnChartValueSelectedListener,OnChartGestureListener,
+        OnChartValueSelectedListener {
 
     // TODO: Connect The Piechart to Line Chart to detuce the selected lead
     // TODO: COnnect the State OF Each Cycle
 
     private String[] leads_name= new String[]{"Lead I","Lead II","Lead III","AVR","AVF","AVL","V1","V2","V3","V4","V5","V6"};
+    private LineChart mChart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +76,80 @@ public class Piechart_Test extends AppCompatActivity implements OnChartValueSele
 
         // TODO :   Line Chart
 
+        mChart = (LineChart) findViewById(R.id.line_chart);
+        mChart.setOnChartGestureListener(this);
+        mChart.setOnChartValueSelectedListener(this);
+        mChart.setDrawGridBackground(false);
 
+        setData();
+        Legend l = mChart.getLegend();
 
+        // modify the legend ...
+        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+        l.setForm(Legend.LegendForm.LINE);
+
+        // no description text
+        mChart.setDescription("Lead Chart");
+        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        // mChart.setScaleXEnabled(true);
+        // mChart.setScaleYEnabled(true);
+
+        LimitLine upper_limit = new LimitLine(130f, "Upper Limit");
+        upper_limit.setLineWidth(4f);
+        upper_limit.enableDashedLine(10f, 10f, 0f);
+        upper_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upper_limit.setTextSize(10f);
+
+        LimitLine lower_limit = new LimitLine(-30f, "Lower Limit");
+        lower_limit.setLineWidth(4f);
+        lower_limit.enableDashedLine(10f, 10f, 0f);
+        lower_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        lower_limit.setTextSize(10f);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(upper_limit);
+        leftAxis.addLimitLine(lower_limit);
+        leftAxis.setAxisMaxValue(220f);
+        leftAxis.setAxisMinValue(-50f);
+        //leftAxis.setYOffset(20f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        mChart.getAxisRight().setEnabled(false);
+
+        //mChart.getViewPortHandler().setMaximumScaleY(2f);
+        //mChart.getViewPortHandler().setMaximumScaleX(2f);
+
+        mChart.animateX(5000, Easing.EasingOption.EaseInOutQuart);
+        mChart.setLayoutAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        //  dont forget to refresh the drawing
+        mChart.invalidate();
 
 
 
